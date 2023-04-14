fc65cfe8c4dbe9ba81967a58b28cf3c33dd80e66
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2023-04-14 03:22:13 -0700

    Delete ECS_032B directory

--- ECS_032B/HW3/ConnectN/ConnectNGame/src/player.py
+++
@@ -1,6 +0,0 @@
-class Player(object):
-
-    def __init__(self, name: str, piece: str, player_number: int):
-        self.name: str = name
-        self.piece: str = piece
-        self.player_num: str = player_number
